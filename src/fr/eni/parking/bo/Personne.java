ff302717de36cf55b762d77d686018cf55e009ca
sun167 <caoson167@gmail.com>
2021-03-08 13:11:26 +0100

    Creation de la classe personne

---
+++ src/fr/eni/parking/bo/Personne.java
@@ -0,0 +1,47 @@
+package fr.eni.parking.bo;
+/**
+ * Represente une voiture
+ */
+public class Personne {
+    private int id;
+    private String nom;
+    private String prenom;
+
+    public Personne(int id, String nom, String prenom) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    @Override
+    public String toString() {
+        return "Personne{" +
+                "nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                '}';
+    }
+}
