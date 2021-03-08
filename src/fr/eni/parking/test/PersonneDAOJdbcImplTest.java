6d81d216b9f07110172b1d186a73928c6961712a
sun167 <caoson167@gmail.com>
2021-03-08 16:23:02 +0100

    Ajouter le test JUnit

---
+++ src/fr/eni/parking/test/PersonneDAOJdbcImplTest.java
@@ -0,0 +1,34 @@
+package fr.eni.parking.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonneDAOJdbcImplTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void selectById() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void selectAll() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void update() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void insert() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void delete() {
+    }
+}
