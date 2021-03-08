18ac0a8d102fc60b7cc0744f56e6d3f1dfa082b7
sun167 <caoson167@gmail.com>
2021-03-08 15:51:45 +0100

    Creation de la classe voiture

---
+++ src/fr/eni/parking/bo/Voiture.java
@@ -0,0 +1,60 @@
+package fr.eni.parking.bo;
+/**
+ * Represente une voiture
+ */
+public class Voiture {
+        private int id;
+        private String nom;
+        private String immatriculation;
+        private Personne conducteur;
+
+        public Voiture(int id, String nom, String immatriculation, Personne conducteur) {
+            this.id = id;
+            this.nom = nom;
+            this.immatriculation = immatriculation;
+            this.conducteur = conducteur;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getNom() {
+            return nom;
+        }
+
+        public void setNom(String nom) {
+            this.nom = nom;
+        }
+
+        public String getImmatriculation() {
+            return immatriculation;
+        }
+
+        public void setImmatriculation(String immatriculation) {
+            this.immatriculation = immatriculation;
+        }
+
+        public Personne getConducteur() {
+            return conducteur;
+        }
+
+        public void setConducteur(Personne conducteur) {
+            this.conducteur = conducteur;
+        }
+
+        @Override
+        public String toString() {
+            return "Voiture{" +
+                    "nom='" + nom + '\'' +
+                    ", immatriculation='" + immatriculation + '\'' +
+                    ", personne=" + conducteur +
+                    '}';
+        }
+}
+
+
