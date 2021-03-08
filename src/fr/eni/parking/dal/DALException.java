9af59864d247520bc4c80a9988d1ecfaa59e4912
sun167 <caoson167@gmail.com>
2021-03-08 15:55:52 +0100

    Creation de la la DALException

---
+++ src/fr/eni/parking/dal/DALException.java
@@ -0,0 +1,30 @@
+
+    package fr.eni.parking.dal;
+
+    public class DALException extends Exception{
+        //Constructeurs
+        public DALException() {
+            super();
+        }
+
+        public DALException(String message) {
+            super(message);
+        }
+
+        public DALException(String message, Throwable exception) {
+            super(message, exception);
+        }
+
+        //Méthodes
+        @Override
+        public String getMessage() {
+            StringBuffer sb = new StringBuffer("Couche DAL - ");
+            sb.append(super.getMessage());
+
+            return sb.toString() ;
+        }
+    }
+
+
+
+
