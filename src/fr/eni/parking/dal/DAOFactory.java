73e89e3ff476c91b94fac0a5af2559e4126110f4
sun167 <caoson167@gmail.com>
2021-03-09 11:11:39 +0100

    Changer le DAO Factory pour produire VoitureDAO

--- src/fr/eni/parking/dal/DAOFactory.java
+++ src/fr/eni/parking/dal/DAOFactory.java
@@ -1,10 +1,15 @@
 package fr.eni.parking.dal;
 
 import fr.eni.parking.dal.jdbc.PersonneDAOJdbcImpl;
+import fr.eni.parking.dal.jdbc.VoitureDAOjdbcImpl;
 
 public class DAOFactory {
     public static PersonneDAO getPersonneDAO() {
         PersonneDAO item = new PersonneDAOJdbcImpl();
+        return item;
+    }
+    public static VoitureDAO getVoitureDAO() {
+        VoitureDAO item = new VoitureDAOjdbcImpl();
         return item;
     }
 }
