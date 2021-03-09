20ff711a51767b12fb60e7cb7d5319c6474ebb7e
sun167 <caoson167@gmail.com>
2021-03-09 08:51:40 +0100

    Changer nom du fichier

--- src/fr/eni/parking/dal/DAOFactory.java
+++ src/fr/eni/parking/dal/DAOFactory.java
@@ -2,8 +2,8 @@ package fr.eni.parking.dal;
 
 import fr.eni.parking.dal.jdbc.PersonneDAOJdbcImpl;
 
-public class PersonneDAOFactory {
-    public static PersonneDAO getArticleDAO() {
+public class DAOFactory {
+    public static PersonneDAO getPersonneDAO() {
         PersonneDAO item = new PersonneDAOJdbcImpl();
         return item;
     }
